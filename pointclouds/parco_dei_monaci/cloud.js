f0dac1a8e67492942787f6aa672618968cf94111
ARC-PRI <arcangelo.priore@unibas.it>
2026-04-02 20:00:01 +0200

    Initial commit

---
+++ pointclouds/parco_dei_monaci/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 10293830,
+    "boundingBox": {
+        "lx": -14.295718475341797,
+        "ly": -38.34036080932617,
+        "lz": -7.512967674255371,
+        "ux": 35.7042815246582,
+        "uy": 11.659639190673829,
+        "uz": 42.48703232574463
+    },
+    "tightBoundingBox": {
+        "lx": -14.295718475341797,
+        "ly": -38.34036080932617,
+        "lz": -7.512967674255371,
+        "ux": 35.7042815246582,
+        "uy": 11.659639190673829,
+        "uz": 7.256032325744629
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "NormalX",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "NormalY",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "NormalZ",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        }
+    ],
+    "spacing": 0.4330126941204071,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
